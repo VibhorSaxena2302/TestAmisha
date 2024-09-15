4d4ce6bf7b3ebe7caffe44a736cdda45e2b9d307
VibhorSaxena2302 <vibhorsaxena2302@gmail.com>
2024-09-16 00:34:21 +0530

    Fix p

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ import Navigation from "@/components/navigation";
 import dynamic from "next/dynamic";
 const Wizard = dynamic(() => import("@/components/models/Wizard"), {
   ssr: false,
-  loading: () => <p>Loading...</p>,
 });
 
 export default function Home() {
